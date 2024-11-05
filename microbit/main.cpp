326f323f2e7c3fda782eaeda9dacc6ef60c8e65b
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-11-05 12:41:39 +0000

    microbitter

--- microbit/main.cpp
+++ microbit/main.cpp
@@ -1,6 +1,22 @@
-#include <MicroBit.h>
+#include "MicroBit.h"
 
 MicroBit uBit;
 
+// Do something on button A being pressed.
+void onButtonA(MicroBitEvent) {
+    //
+    int heading = uBit.compass.heading();
+    if (heading == DEVICE_CALIBRATION_IN_PROGRESS)
+        return;
+
+    uBit.display.scroll(heading);
+}
+
 int main() {
     uBit.init();
+    uBit.messageBus.listen(
+        MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA
+    );
+
+    release_fiber();
+}
