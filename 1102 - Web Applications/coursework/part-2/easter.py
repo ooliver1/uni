b630df7c135baf9de78a1a0015f110eaa3bad7d5
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-11-26 16:53:00 +0000

    coursework pt 2 finally started

--- 1102 - Web Applications/coursework/part-2/easter.py
+++ 1102 - Web Applications/coursework/part-2/easter.py
@@ -4,5 +4,33 @@ import cgitb
 
 cgitb.enable()
 
-print("Content-type: text/html")
+form = cgi.FieldStorage()
+year = form.getvalue("year")
+
+
+with open("../C1102/part-2/index.html", "r") as f:
+    html = f.read()
+
+
+def get_easter(year):
+    a = year % 19
+    b = year // 100
+    c = year % 100
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    m = (a + 11 * h + 22 * l) // 451
+    month = (h + l - 7 * m + 114) // 31
+    day = ((h + l - 7 * m + 114) % 31) + 1
+    return f"{day}/{month}/{year}"
+
+
+html = html.replace("<!-- easter date -->", get_easter(int(year)))
+print("Content-Type: text/html")
 print()
+print(html)
