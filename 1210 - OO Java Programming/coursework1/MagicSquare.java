82bb437ba6e2ef699b8f5e1a6dbcb2effcf34d49
Oliver Wilkes <oliverwilkes2006@icloud.com>
2025-03-06 12:49:32 +0000

    h

---
+++ 1210 - OO Java Programming/coursework1/MagicSquare.java
@@ -0,0 +1,46 @@
+public class MagicSquare {
+    private int[][] grid;
+    private int size;
+    private int row;
+    private int column;
+
+    public MagicSquare(int size) {
+        this.size = size;
+        grid = new int[size][size];
+
+        row = 0;
+        column = size / 2;
+
+        grid[row][column] = 1;
+
+        for (int i = 2; i <= size * size; i++) {
+            int newRow = (row - 1 + size) % size;
+            int newColumn = (column - 1 + size) % size;
+
+            if (grid[newRow][newColumn] == 0) {
+                row = newRow;
+                column = newColumn;
+            } else {
+                row = (row + 1) % size;
+            }
+
+            grid[row][column] = i;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String format() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                sb.append(grid[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
--- 1210 - OO Java Programming/coursework1/MagicSquareGenerator.java
+++ 1210 - OO Java Programming/coursework1/MagicSquareGenerator.java
@@ -6,45 +6,8 @@ public class MagicSquareGenerator {
         }
 
         int size = Integer.parseInt(args[0]);
-        if (size < 1 || size % 2 == 0) {
-            System.out.println("Size must be a positive odd number.");
-            return;
-        }
+        MagicSquare magicSquare = new MagicSquare(size);
 
-        int[][] magicSquare = new int[size][size];
-
-        int row = 0;
-        int column = size / 2;
-
-        for (int i = 1; i <= size * size; i++) {
-            // Add another size to ensure this is not negative.
-            int newRow = (row - 1 + size) % size;
-            int newColumn = (column + 1) % size;
-            
-            if (magicSquare[newRow][newColumn] != 0) {
-                newRow = (row + 1) % size;
-                newColumn = column;
-            }
-
-            magicSquare[row][column] = i;
-            row = newRow;
-            column = newColumn;
-        }
-
-        System.out.print(display(magicSquare));
-    }
-
-    private static String display(int[][] magicSquare) {
-        int padding = (int) Math.log10(magicSquare.length * magicSquare.length) + 1;
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < magicSquare.length; i++) {
-            for (int j = 0; j < magicSquare[i].length; j++) {
-                sb.append(String.format("%" + padding + "d", magicSquare[i][j]));
-                sb.append(" ");
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
+        System.out.print(magicSquare.format());
     }
 }
